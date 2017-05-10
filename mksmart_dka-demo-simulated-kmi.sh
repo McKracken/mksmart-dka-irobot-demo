d9c1c117ccd02c3471a8582dbf80f689c4f76775
kmitd <ilaria.tiddi@open.ac.uk>
2017-05-10 14:35:55 +0100

    changes in launch script

--- mksmart_dka-demo-simulated-kmi.sh
+++ mksmart_dka-demo-simulated-kmi.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-echo "hello"
-
 # source kmi_catkin workspace
-source kmi_catkin_ws/devel/setup.bash
+source /home/ilaria/kmi_catkin_ws/devel/setup.bash
+echo "setup.bash sourced"
 
 # launch stage simulation (will open a new wiindow with the map)
 xterm -e roslaunch kmi_navigation stage_kmi.launch &
-echo "started Stage sim"
+echo "stage started"
 
 sleep 3
 #launch move_base
 xterm -e roslaunch kmi_navigation move_base_kmi_simulated.launch &
-echo "started Move_base"
+echo "move_base started"
 sleep 2
 
 #start the robot server
-nohup python /home/ilaria/kmi_catkin_ws/src/kmi_ros/dynamic_knowledge_acquisition/scripts/robot_server.py -w eduroam -p 8080 > robot_server.log &
-echo "started the robot server (set up for eduroam / KBserverport 8080) "
+#nohup python /home/ilaria/kmi_catkin_ws/src/kmi_ros/dynamic_knowledge_acquisition/scripts/robot_server.py -w eduroam -p 8080 > robot_server.log &
+#echo "started the robot server (set up for eduroam / KBserverport 8080) "
 
 sleep 2
 
 # launch dka-server
 cd /home/ilaria/workspace/dka-robo/server
 nohup ./run.sh -l KB_partial_kmi_simulated.nq > server.log &
-echo "started dka server"
+echo "dka-server started"
 
 sleep 2
 
 # set bot
 ./dka.sh setbot http://localhost:5000
-echo "Set bot at localhost:5000"
+echo "set bot at localhost:5000"
 
 sleep 2
 
